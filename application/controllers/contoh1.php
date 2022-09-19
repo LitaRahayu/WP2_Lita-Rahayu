0da5d8f3bdd0fe2a0ec4e9435a76e182ff2d0d47
Lita Rahayu <littarahayyu@gmail.com>
2022-09-19 18:40:13 +0700

    Tambah contoh1.php

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+     public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Lita Rahayu Ningsih
+            Saya tingga di daerah Pulo Gadung
+            olah raga yang saya sukai adalah
+            volly";
+    }
+}
